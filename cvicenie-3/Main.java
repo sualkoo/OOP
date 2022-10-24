007f4cae6627c99217917aff0dc95da902682133
sualkoo <sualkoo@gmail.com>
2022-10-24 19:18:38 +0200

    feat: implementing array of students

--- cvicenie-3/Main.java
+++ cvicenie-3/Main.java
@@ -1,8 +1,29 @@
-import Student;
+import Student.Student;
+
+import java.util.Scanner;
+import java.util.concurrent.ThreadLocalRandom;
 
 public class Main {
     public static void main(String[] args) {
-        Student student = new Student(1, "Jozef", "Olejnik", 25);
-        student.get
+        Scanner scanner = new Scanner(System.in);
+        Student[] student = new Student[3];
+
+        //3.1 filling array of students
+        for (int i = 0; i < 3; i++) {
+            System.out.println("Type name: ");
+            String name  = scanner.next();
+            System.out.println("Type surname: ");
+            String surname  = scanner.next();
+            int random = ThreadLocalRandom.current().nextInt(18,40);
+            student[i] = new Student();
+            student[i].setId(i);
+            student[i].setName(name);
+            student[i].setSurname(surname);
+            student[i].setAge(random);
+        }
+
+        //3.2 TODO sorting the array
+
+
     }
 }
--- cvicenie-3/Student/Student.java
+++ cvicenie-3/Student/Student.java
@@ -1,48 +1,48 @@
-private class Student
+package Student;
+public class Student
 {
     int id;
     String name;
     String surname;
     int age;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public Student() {
+        this.id = id;
+        this.name = name;
+        this.surname = surname;
+        this.age = age;
+    }
 }
 
-
-public Student(int id, String name, String surname, int age) {
-    this.id = id;
-    this.name = name;
-    this.surname = surname;
-    this.age = age;
-}
-
-
-public int getId() {
-    return this.id;
-}
-
-public void setId(int id) {
-    this.id = id;
-}
-
-public String getName() {
-    return this.name;
-}
-
-public void setName(String name) {
-    this.name = name;
-}
-
-public String getSurname() {
-    return this.surname;
-}
-
-public void setSurname(String surname) {
-    this.surname = surname;
-}
-
-public int getAge() {
-    return this.age;
-}
-
-public void setAge(int age) {
-    this.age = age;
-}
